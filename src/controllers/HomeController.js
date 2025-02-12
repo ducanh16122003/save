116496bb35f5f440ced8e48a5d72e3e849922932
Đức Anh Nguyễn <ducanh16122003@gmail.com>
2025-02-12 16:57:23 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -72,9 +72,38 @@ function handleMessage(sender_psid, received_message){
     //check if the message contains text
     if (received_message.text) {
 
-        //create the payload for a basic text message
+        //create the payload for a basic text message, which
+        //will be added to the body of our request to the Send API
         response = {
             "text": `you sent the message: "${received_message.text}". Now send me an image!`
+        }
+    } else if (received_message.attachments){
+        //gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
         }
     }
     //sends the response message
