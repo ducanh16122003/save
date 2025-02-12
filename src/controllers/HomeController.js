2842fa59a1cdc041b699c62f59241a073abc6d8e
Đức Anh Nguyễn <ducanh16122003@gmail.com>
2025-02-12 19:57:40 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -78,6 +78,7 @@ function handleMessage(sender_psid, received_message){
             "text": `you sent the message: "${received_message.text}". Now send me an image!`
         }
     } else if (received_message.attachments){
+        
         //gets the URL of the message attachment
         let attachment_url = received_message.attachments[0].payload.url;
         response = {
