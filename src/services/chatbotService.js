5272cd4351952e08a7926cbae87697edf5209af6
Đức Anh Nguyễn <ducanh16122003@gmail.com>
2025-03-04 02:08:49 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -29,28 +29,9 @@ let callSendAPI = (sender_psid, response) => {
 })
 }
 
-let getUserName = (sender_psid) => {
-    return new Promise((resolve, reject) => {
-        request({
-            "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${page_access_token}`,
-            "method": "GET",
-        }, (err, res, body) => {
-            if (!err) {
-                body = JSON.parse(body);
-                let userName = `${body.first_name} ${body.last_name}`;
-                resolve(userName);
-            } else {
-                console.error("Unable to send message:" + err);
-                reject(err);
-            }
-        });
-    })
-}
-
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
     try {
-        let userName = await getUserName(sender_psid);
         let response = { "text": `Chào mừng đến với bình nguyên vô tận, ${userName}!` }
         await callSendAPI(sender_psid, response);
         resolve('done');
