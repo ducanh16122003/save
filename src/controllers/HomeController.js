f450f2abfd8c03fcf0c34028477e696daa071b30
Đức Anh Nguyễn <ducanh16122003@gmail.com>
2025-03-03 13:07:55 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -120,12 +120,19 @@ function handlePostback(sender_psid, received_postback){
     let payload = received_postback.payload;
 
     //set the respose based on the postback payload
-    if (payload === 'yes') {
-        response = {"text": "Thanks!"}
-    } else if (payload === 'no') {
-        response = {"text": "Oops, try sending another image."}
-    } else if (payload === "GET_STARTED") {
-        response = {"text": "OK. Xin chào bạn XYZ đến với nhà hàng của Bli"}
+    switch (payload) {
+        case 'yes':
+            response = {"text": "Thanks!"}
+            break;
+        case 'no':
+            response = {"text": "Oops, try sending another image."}
+            break;
+        case 'GET_STARTED':
+            response = {"text": "OK. Xin chào bạn ABC đến với nhà hàng của Bli"}
+            break;
+        default:
+            response = {"text": `oops! I don't know response with postback ${payload}`}
+        
     }
     //Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
