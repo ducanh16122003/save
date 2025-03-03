900c5f7457f8c8d092cee2c010549dbff0c3f989
Đức Anh Nguyễn <ducanh16122003@gmail.com>
2025-03-03 09:47:47 +0700

    update

---
+++ src/services/chatbotService.js
@@ -0,0 +1,35 @@
+import request from "request";
+require('dotenv').config();
+
+const page_access_token = process.env.PAGE_ACCESS_TOKEN;
+let callSendAPI = () => {
+    //Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+    
+    //Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v21.0/me/messages",
+        "qs" : { "access_token": page_access_token },
+        "method": "POST",
+        "headers": { "Content-Type": "application/json" },
+        "json": request_body
+    },(err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("unable to send message:" + err);
+        }
+    })
+}
+let handleGetStarted = () => {
+
+}
+
+module.exports = {
+    handleGetStarted: handleGetStarted
+}
